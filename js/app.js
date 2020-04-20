073b5ad52a735fc82187bb50e51cdb249cbdbd35
SmavaProject <katyakravchenka@yahoo.com>
2020-04-20 09:34:50 -0700

    changed game constructor

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 const startGameBtn = document.getElementById("btn__reset");
 const letters = document.querySelectorAll(".key");
-const game = new Game(1, 1, "lala");
-
+const game = new Game();
+/***
+ * Start game
+ */
 startGameBtn.addEventListener('click', e =>{
     game.startGame();
 });
-
+/***
+ * Handle button clicks
+ */
 letters.forEach(letter =>{
     letter.addEventListener('click', e =>{
-        //const lett = e.target.textContent;
         const lett = e.target;
         game.handleInteraction(lett);
     });
